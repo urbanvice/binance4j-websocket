509b7bbb67436a6a1910a19c51a80fef664a2b39
vivienld <leducvivien@gmail.com>
2022-04-05 14:46:01 +0200

    published

--- src/main/java/com/binance4j/websocket/client/WebsocketAllTickersClient.java
+++ src/main/java/com/binance4j/websocket/client/WebsocketAllTickersClient.java
@@ -6,7 +6,7 @@ import com.binance4j.websocket.payload.TickerPayload;
  * Websocket client handling ticker events on all
  * symbols.
  * 
- * @Deprecated No data returned when testing the endpoint
+ * @deprecated No data returned when testing the endpoint
  */
 public class WebsocketAllTickersClient extends WebSocketClient<TickerPayload> {
     /**
